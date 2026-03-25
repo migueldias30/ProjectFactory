42135fe1a1ef576ee0b71bcf4cb323660de443fd
migueldias30 <150258425+migueldias30@users.noreply.github.com>
2026-03-25 11:39:56 +0000

    .

---
+++ include/User_Setup.h
@@ -0,0 +1,20 @@
+#define TFT_WIDTH  128
+#define TFT_HEIGHT 128
+
+#define TFT_CS   5
+#define TFT_DC   2
+#define TFT_RST  4
+
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_MISO 19
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+#define SMOOTH_FONT
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
 
 // put function declarations here:
-int myFunction(int, int);
+TFT_eSPI tft = TFT_eSPI();
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  tft.init();
+  tft.setRotation(1); // Adjust rotation if needed
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.println("Display WPSH412 funcionando!");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(0, 0);
+  tft.println("Texto atualizado");
+  tft.println(millis() / 1000);
 }
